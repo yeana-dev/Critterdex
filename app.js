7c607c7e67d1872b226f0d4caccaba07cf9813d9
yeana0324 <yeana.dev@gmail.com>
2021-08-02 13:46:32 -0500

    🥝 Render search result to HTML

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const seaUrl = "http://acnhapi.com/v1a/sea";
 const searchInput = document.querySelector(".search-form");
 const searchBar = document.querySelector("#search-bar");
 
+const resultDiv = document.querySelector(".results");
+
 const getData = async (searchValue) => {
   try {
     const bugsData = await axios.get(bugsUrl);
@@ -16,8 +18,41 @@ const getData = async (searchValue) => {
     const listOfFish = fishData.data;
     const listOfSea = seaData.data;
 
+    // Merging all three datas into one data
+    // It will be easier to display results without categoring
     const totalList = listOfBugs.concat(listOfFish).concat(listOfSea);
-    console.log(totalList);
+    const filteredSearch = totalList.filter((critter) => {
+      return critter.name["name-USen"].includes(searchValue);
+    });
+    console.log(filteredSearch);
+
+    //Rendering Results to HTML using DOM
+    filteredSearch.forEach((result) => {
+      //icon
+      const icon = document.createElement("img");
+      icon.setAttribute("src", result.icon_uri);
+      resultDiv.append(icon);
+
+      //name
+      const name = document.createElement("div");
+      name.textContent = result.name["name-USen"];
+      resultDiv.append(name);
+
+      //location
+      const location = document.createElement("div");
+      location.textContent = result.availability.location;
+      resultDiv.append(location);
+
+      //month
+      const months = document.createElement("div");
+      months.textContent = `North: ${result.availability["month-northern"]} / South : ${result.availability["month-southern"]}`;
+      resultDiv.append(months);
+
+      //time
+      const time = document.createElement("div");
+      time.textContent = result.availability.time;
+      resultDiv.append(time);
+    });
   } catch (error) {
     console.error(error);
   }
@@ -25,5 +60,6 @@ const getData = async (searchValue) => {
 
 searchInput.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(searchBar.value);
   getData(searchBar.value);
 });
