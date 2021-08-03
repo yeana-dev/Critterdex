d1432eaa5bb1ca0f7fffc2f21d2467f72b5977e2
yeana0324 <yeana.dev@gmail.com>
2021-08-03 10:22:42 -0500

    🍇 Print available all day/year

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const seaUrl = "http://acnhapi.com/v1a/sea";
 const searchInput = document.querySelector(".search-form");
 const searchBar = document.querySelector("#search-bar");
 
-const resultDiv = document.querySelector(".results");
+const resultContainer = document.querySelector(".result-container");
 
 const getData = async (searchValue) => {
   try {
@@ -19,42 +19,66 @@ const getData = async (searchValue) => {
     const listOfSea = seaData.data;
 
     // Merging all three datas into one data
-    // It will be easier to display results without categoring
+    // It will be easier to display results without categorizing
     const totalList = listOfBugs.concat(listOfFish).concat(listOfSea);
     const filteredSearch = totalList.filter((critter) => {
       return critter.name["name-USen"].includes(searchValue);
     });
+
     console.log(filteredSearch);
 
-    // Remove previous search
-    removePrevious(resultDiv);
+    // Remove previous search before rendering new search
+    removePrevious(resultContainer);
 
     //Rendering Results to HTML using DOM
     filteredSearch.forEach((result) => {
+      // Making an individual div for each result
+      const resultDiv = document.createElement("div");
+      resultDiv.classList.add("resultDiv");
+      resultContainer.append(resultDiv);
+
+      // Making results as list
+      const resultContent = document.createElement("ul");
+      resultContainer.append(resultContent);
+
       //icon
       const icon = document.createElement("img");
+      icon.classList.add("iconImg");
       icon.setAttribute("src", result.icon_uri);
-      resultDiv.append(icon);
+      resultContent.append(icon);
 
       //name
-      const name = document.createElement("div");
+      const name = document.createElement("li");
       name.textContent = result.name["name-USen"];
-      resultDiv.append(name);
+      name.classList.add("name");
+      resultContent.append(name);
 
       //location
-      const location = document.createElement("div");
+      const location = document.createElement("li");
       location.textContent = result.availability.location;
-      resultDiv.append(location);
+      resultContent.append(location);
 
       //month
-      const months = document.createElement("div");
-      months.textContent = `North: ${result.availability["month-northern"]} / South : ${result.availability["month-southern"]}`;
-      resultDiv.append(months);
+      const months = document.createElement("li");
+      // if the critter is available all year, print
+      if (result.availability.isAllYear === true) {
+        months.textContent = "Available All Year";
+      } else {
+        // if not, print the months of availability, on each hemisphere
+        months.textContent = `North: ${result.availability["month-northern"]} / South : ${result.availability["month-southern"]}`;
+      }
+      resultContent.append(months);
 
       //time
-      const time = document.createElement("div");
-      time.textContent = result.availability.time;
-      resultDiv.append(time);
+      const time = document.createElement("li");
+      // if the critter is available all day, print
+      if (result.availability.isAllDay === true) {
+        time.textContent = "Available All Day";
+      } else {
+        // if not, print the time of availability
+        time.textContent = result.availability.time.toUpperCase();
+      }
+      resultContent.append(time);
     });
   } catch (error) {
     console.error(error);
@@ -66,7 +90,6 @@ searchInput.addEventListener("submit", (e) => {
   if (searchBar.value.length == 0) {
     // Stopping the function from re-rendering whole page with entire list of critters
     // when user click the submit button with empty input.
-    alert("Please search by name!");
     return;
   }
   getData(searchBar.value);
