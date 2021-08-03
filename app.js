0f56010ae736d3c46ec29e7ff10b595332fd6555
yeana0324 <yeana.dev@gmail.com>
2021-08-03 14:02:57 -0500

    🍅 Categorized the results by types of critters with headers

--- app.js
+++ app.js
@@ -14,93 +14,48 @@ const getData = async (searchValue) => {
     const fishData = await axios.get(fishUrl);
     const seaData = await axios.get(seaUrl);
 
-    const listOfBugs = bugsData.data;
-    const listOfFish = fishData.data;
-    const listOfSea = seaData.data;
+    // const listOfBugs = bugsData.data;
+    // const listOfFish = fishData.data;
+    // const listOfSea = seaData.data;
 
     // Merging all three datas into one data
     // It will be easier to display results without categorizing
-    const totalList = listOfBugs.concat(listOfFish).concat(listOfSea);
-    const filteredSearch = totalList.filter((critter) => {
+    // const totalList = listOfBugs.concat(listOfFish).concat(listOfSea);
+
+    const bugs = bugsData.data.filter((critter) => {
       return critter.name["name-USen"].includes(searchValue);
     });
-
-    console.log(filteredSearch);
+    const fish = fishData.data.filter((critter) => {
+      return critter.name["name-USen"].includes(searchValue);
+    });
+    const sea = seaData.data.filter((critter) => {
+      return critter.name["name-USen"].includes(searchValue);
+    });
 
     // Remove previous search before rendering new search
     removePrevious(resultContainer);
 
     //Rendering Results to HTML using DOM
-    filteredSearch.forEach((result) => {
-      // Making an individual div for each result
-      const resultDiv = document.createElement("div");
-      resultDiv.classList.add("resultDiv");
-      resultContainer.append(resultDiv);
-
-      // icon
-      const icon = document.createElement("img");
-      icon.classList.add("iconImg");
-      icon.setAttribute("src", result.icon_uri);
-      resultDiv.append(icon);
-
-      // Making separate container for right side content(name,year,location,time)
-      const rightDiv = document.createElement("div");
-      rightDiv.classList.add("right-div");
-      resultDiv.append(rightDiv);
-
-      //name
-      const name = document.createElement("div");
-      name.classList.add("name");
-      name.textContent = result.name["name-USen"];
-      rightDiv.append(name);
-
-      //price
-      const price = document.createElement("div");
-      price.classList.add("price");
-      price.innerHTML = `${result.price} bell`;
-      name.append(price);
-
-      // Wrapping year, location, time into a list
-      const resultContent = document.createElement("ul");
-      rightDiv.append(resultContent);
-
-      // month
-      const months = document.createElement("li");
-      months.classList.add("months");
-      // if the critter is available all year, print
-      if (result.availability.isAllYear === true) {
-        months.innerHTML =
-          '<i class="fas fa-calendar-alt"></i> Available All Year';
-      } else {
-        // if not, print the months of availability, on each hemisphere
-        months.innerHTML =
-          '<i class="fas fa-calendar-alt"></i>' +
-          ` North: ${result.availability["month-northern"]} | South : ${result.availability["month-southern"]}`;
-      }
-      resultContent.append(months);
-
-      //time
-      const time = document.createElement("li");
-      time.classList.add("time");
-      // if the critter is available all day, print
-      if (result.availability.isAllDay === true) {
-        time.innerHTML = '<i class="far fa-clock"></i> Available all day';
-      } else {
-        // if not, print the time of availability
-        time.innerHTML =
-          '<i class="far fa-clock"></i>' +
-          ` ${result.availability.time.toUpperCase()}`;
-      }
-
-      //location
-      const location = document.createElement("li");
-      location.classList.add("location");
-      // Adding location icon
-      location.innerHTML =
-        '<i class="fas fa-map-pin"></i>' + ` ${result.availability.location}`;
-      resultContent.append(location);
-
-      resultContent.append(time);
+    //If no critters were found on search, no title.
+    if (bugs[0] !== undefined) {
+      resultTitleBug();
+    }
+    bugs.forEach((result) => {
+      renderResults(result);
+    });
+    //If no critters were found on search, no title.
+    if (fish[0] !== undefined) {
+      resultTitleFish();
+    }
+    fish.forEach((result) => {
+      renderResults(result);
+    });
+    //If no critters were found on search, no title.
+    if (sea[0] !== undefined) {
+      resultTitleSea();
+    }
+    sea.forEach((result) => {
+      renderResults(result);
     });
   } catch (error) {
     console.error(error);
@@ -121,4 +76,97 @@ function removePrevious(result) {
   while (result.lastChild) {
     result.removeChild(result.lastChild);
   }
+}
+
+// DOM Rendering search result
+function renderResults(result) {
+  // Making an individual div for each result
+  const resultDiv = document.createElement("div");
+  resultDiv.classList.add("resultDiv");
+  resultContainer.append(resultDiv);
+
+  // icon
+  const icon = document.createElement("img");
+  icon.classList.add("iconImg");
+  icon.setAttribute("src", result.icon_uri);
+  resultDiv.append(icon);
+
+  // Making separate container for right side content(name,year,location,time)
+  const rightDiv = document.createElement("div");
+  rightDiv.classList.add("right-div");
+  resultDiv.append(rightDiv);
+
+  //name
+  const name = document.createElement("div");
+  name.classList.add("name");
+  name.textContent = result.name["name-USen"];
+  rightDiv.append(name);
+
+  //price
+  const price = document.createElement("div");
+  price.classList.add("price");
+  price.innerHTML = `${result.price} bell`;
+  name.append(price);
+
+  // Wrapping year, location, time into a list
+  const resultContent = document.createElement("ul");
+  rightDiv.append(resultContent);
+
+  // month
+  const months = document.createElement("li");
+  months.classList.add("months");
+  // if the critter is available all year, print
+  if (result.availability.isAllYear === true) {
+    months.innerHTML = '<i class="fas fa-calendar-alt"></i> Available All Year';
+  } else {
+    // if not, print the months of availability, on each hemisphere
+    months.innerHTML =
+      '<i class="fas fa-calendar-alt"></i>' +
+      ` North: ${result.availability["month-northern"]} | South : ${result.availability["month-southern"]}`;
+  }
+  resultContent.append(months);
+
+  //time
+  const time = document.createElement("li");
+  time.classList.add("time");
+  // if the critter is available all day, print
+  if (result.availability.isAllDay === true) {
+    time.innerHTML = '<i class="far fa-clock"></i> Available all day';
+  } else {
+    // if not, print the time of availability
+    time.innerHTML =
+      '<i class="far fa-clock"></i>' +
+      ` ${result.availability.time.toUpperCase()}`;
+  }
+
+  //location
+  const location = document.createElement("li");
+  location.classList.add("location");
+  // Adding location icon
+  location.innerHTML =
+    '<i class="fas fa-map-pin"></i>' + ` ${result.availability.location}`;
+  resultContent.append(location);
+
+  resultContent.append(time);
+}
+
+function resultTitleBug() {
+  const resultTitle = document.createElement("div");
+  resultTitle.classList.add("result-title");
+  resultTitle.textContent = "Bugs";
+  resultContainer.append(resultTitle);
+}
+
+function resultTitleFish() {
+  const resultTitle = document.createElement("div");
+  resultTitle.classList.add("result-title");
+  resultTitle.textContent = "Fish";
+  resultContainer.append(resultTitle);
+}
+
+function resultTitleSea() {
+  const resultTitle = document.createElement("div");
+  resultTitle.classList.add("result-title");
+  resultTitle.textContent = "Sea Creatures";
+  resultContainer.append(resultTitle);
 }
